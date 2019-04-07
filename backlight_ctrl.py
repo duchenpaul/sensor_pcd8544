1c9b0c5633c1487a10d7e484a96330583bcc838d
duchenpaul <duchenpaul@gmail.com>
2019-04-07 22:53:51 +0800

    Change variable and add dead loog

--- backlight_ctrl.py
+++ backlight_ctrl.py
@@ -5,12 +5,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
 
 
-def backlight_ctrl(BRIGHTNESS):
-    '''BRIGHTNESS must have a value from 0.0 to 100.0'''
+def backlight_ctrl(brightness):
+    '''brightness must have a value from 0.0 to 100.0'''
     pwm = GPIO.PWM(4, 50)  # channel=12 frequency=50Hz
     pwm.start(0)
     try:
-        pwm.ChangeDutyCycle(100 - BRIGHTNESS)
+        while True:
+            pwm.ChangeDutyCycle(100 - brightness)
     except Exception as e:
         raise
     else:
@@ -20,5 +21,5 @@ def backlight_ctrl(BRIGHTNESS):
         GPIO.cleanup()
 
 if __name__ == '__main__':
-    BRIGHTNESS = 0
-    backlight_ctrl(BRIGHTNESS)
+    brightness = 0
+    backlight_ctrl(brightness)
